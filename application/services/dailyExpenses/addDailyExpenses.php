fb0e70454f23d8adfe65bc59085c168c046e5a5f
anusit1234 <not_anusit@hotmail.com>
2018-03-26 17:55:09 +0700

    เพิ่ม service

---
+++ application/services/dailyExpenses/addDailyExpenses.php
@@ -0,0 +1,47 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+$db = new database();
+
+$data = file_get_contents("php://input");   //รับค่าจาก ionic
+
+$objData = json_decode($data);              //แปลง json เป็น object
+
+$user_id = $objData->data->user_id;
+$daily_expenses_cate_id = $objData->data->daily_expenses_cate_id;
+$amount = $objData->data->amount;
+$payment_channel_id = $objData->data->payment_channel_id;
+$images = $objData->data->images;
+$created = $objData->data->created;
+    
+
+// $db = new database();
+    // $sql = "SELECT name FROM users where email='$email' and permission='member'";
+    // $query = $db->query($sql);
+    // $rows = $db->rows($query);
+
+    // if($rows > 0){
+    //     echo json_encode(null);
+    // }
+    // else{
+        $value_due_date = array(
+            "user_id" => $user_id,
+            "fix_expenses_cate_id" => $fix_expenses_cate_id,
+            "payment_channel_id" => $payment_channel_id,
+            "amount" => $amount,
+            "images" => $images,
+            "created" => $created
+        );
+        $query_due_date = $db->insert("daily_expenses", $value_due_date);
+
+        if ($query_due_date == TRUE) {
+
+            echo json_encode("success");
+        }
+        else{
+            echo json_encode(null);
+        }
+    // }
+
+?>
--- application/services/users/getUserById.php
+++ application/services/users/getUserById.php
@@ -11,16 +11,21 @@ $password = $_GET['password'];
 $sql = "SELECT * FROM users where email='$email' and password='$password' and permission='member'";
 
 $query = $db->query($sql);
-
 $num = mysqli_num_rows($query);
 // echo $num;
+$rows = $db->rows($query);
+if($rows  == 0){
+    echo json_encode(null);
+}
+else{
 
-$arr = array();
-if($num > 0){
-    while($rows = mysqli_fetch_assoc($query)){
-        $arr[] = $rows;
+    $arr = array();
+    if($num > 0){
+        while($rows = mysqli_fetch_assoc($query)){
+            $arr[] = $rows;
+        }
+        echo json_encode($arr[0]);
     }
-    echo json_encode($arr[0]);
 }
 // else{
 //      echo null;
