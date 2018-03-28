815bcd77eb290d7241f42fd6d1741c6cccbcc016
anusit1234 <not_anusit@hotmail.com>
2018-03-28 16:47:13 +0700

    service

--- application/services/income/getIncome.php
+++ application/services/income/getIncome.php
@@ -6,11 +6,12 @@ $db = new database();
 
 // $i = 3 ;   //test id
 $id = $_GET['user_id'];
+$month = $_GET['month'];
 
 //ดึงข้อมูลรายการรายรับทั้งหมด
 $sql = "SELECT t2.name as category, t1.amount, t1.created
             FROM income t1 inner join income_categories t2 on t1.income_cate_id = t2.id
-            WHERE t1.user_id = '$id' order by t1.created DESC
+            WHERE t1.user_id = '$id' and  MONTH(t1.created)='$month'
             ";
 
 $query = $db->query($sql);
